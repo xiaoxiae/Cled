3f8782bbab7ff8e4dbe868640e3ade2688e7ff2b
xiaoxiae <tomas@slama.dev>
2022-03-02 19:12:55 +0100

    movement: up/down movement
    Done for now.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 10f;
+    public float speed = 7f;
 
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        
+
+        bool spacePressed = Input.GetKey(KeyCode.Space);
+        bool ctrlPressed = Input.GetKey(KeyCode.LeftControl);
+
+        float upDownSpeed = speed * 0.03f;
+
         float adjustedSpeed = speed;
         if (Input.GetKey(KeyCode.LeftShift))
-            adjustedSpeed *= 1.5f;
+            adjustedSpeed *= 1.6f;
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * x + transform.forward * z +
+                       transform.up * ((spacePressed ? upDownSpeed : 0) + (ctrlPressed ? -upDownSpeed : 0));
 
         controller.Move(move * (adjustedSpeed * Time.deltaTime));
     }
