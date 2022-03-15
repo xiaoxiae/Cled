2fab50685695f206ff7b8e2f8362d09d992c55d2
xiaoxiae <tomas@slama.dev>
2022-03-15 18:54:30 +0100

    Proper hold highlighting when hover

--- Assets/PlayerSelection.cs
+++ Assets/PlayerSelection.cs
@@ -14,41 +14,44 @@ public class PlayerSelection : MonoBehaviour
     private int _selectedIndex;
 
     // the current hold object
-    private GameObject pointerObject;
+    private GameObject _selectedObject;
     
-    // where the pointer object looked previously
-    private Vector3 previousPointerObjectNormal = Vector3.zero;
+    // where the currently selected object pointed previously
+    private Vector3 _previousSelectedObjectNormal = Vector3.zero;
 
     // the angle of the pointer object (from 0 to 2 pi)
-    private float pointerObjectRotation = 0;
+    private float _selectedObjectRotation;
 
 	public float mouseSensitivity = 1f;
+	
+    // the object that was last hovered
+    // used for removing the outline when it is no longer hoverd
+    private GameObject previouslyHoveredObject;
     
-    private bool wasSeen; 
     void Start()
     {
         // TODO: remove this, temporarily selects all of the holds
         _selected = HoldManager.Filter(hold => true);
         
-        pointerObject = HoldManager.ToGameObject(_selected[_selectedIndex]);
-        pointerObject.layer = 2;  // ignore this object until placed
+        _selectedObject = HoldManager.ToGameObject(_selected[_selectedIndex]);
+        _selectedObject.layer = 2;  // ignore this object until placed
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && PlayerControl.CurrentMode == Mode.Edit)
         {
-            pointerObject.layer = 0;
-            StateManager.AddHold(pointerObject);
-            pointerObject = Instantiate(pointerObject);
-            pointerObject.layer = 2;
+            _selectedObject.layer = 0;
+            StateManager.AddHold(_selectedObject);
+            _selectedObject = Instantiate(_selectedObject);
+            _selectedObject.layer = 2;
         }
 
         if (Input.GetKey(KeyCode.LeftShift) && PlayerControl.CurrentMode == Mode.Edit)
         {
             float x = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
 
-            pointerObjectRotation = (pointerObjectRotation + x) % (2 * (float)Math.PI);
+            _selectedObjectRotation = (_selectedObjectRotation + x) % (2 * (float)Math.PI);
         }
         
         // cast a ray directly in front of the camera
@@ -59,21 +62,21 @@ public class PlayerSelection : MonoBehaviour
         {
             if (PlayerControl.CurrentMode == Mode.Edit)
             {
-                pointerObject.transform.Rotate(0.1f, 0f, 0f);
+                _selectedObject.transform.Rotate(0.1f, 0f, 0f);
                 
                 // for smooth movement of the object that we currently have selected
-                pointerObject.transform.position = Vector3.Lerp(pointerObject.transform.position, hit.point,
-                    pointerObject.activeSelf ? 0.55f : 1f);
+                _selectedObject.transform.position = Vector3.Lerp(_selectedObject.transform.position, hit.point,
+                    _selectedObject.activeSelf ? 0.55f : 1f);
                 
-                var currentNormal = Vector3.Lerp(previousPointerObjectNormal, hit.normal, 0.3f);
+                var currentNormal = Vector3.Lerp(_previousSelectedObjectNormal, hit.normal, 0.3f);
                 
                 // rotate the world "up" around the hit normal by some degrees
-                var upVector = Quaternion.AngleAxis(Mathf.Rad2Deg * pointerObjectRotation, hit.normal) * Vector3.up;
+                var upVector = Quaternion.AngleAxis(Mathf.Rad2Deg * _selectedObjectRotation, hit.normal) * Vector3.up;
                 
-                pointerObject.transform.LookAt(currentNormal + hit.point, upVector);
-                pointerObject.SetActive(true);
+                _selectedObject.transform.LookAt(currentNormal + hit.point, upVector);
+                _selectedObject.SetActive(true);
 
-                previousPointerObjectNormal = currentNormal;
+                _previousSelectedObjectNormal = currentNormal;
             }
             else
             {
@@ -88,16 +91,34 @@ public class PlayerSelection : MonoBehaviour
                         outline.OutlineMode = Outline.Mode.OutlineAll;
                         outline.OutlineColor = Color.white;
                         outline.OutlineWidth = 15f;
+
+                        previouslyHoveredObject = hitObject;
                     }
                 }
                 else
-                    pointerObject.SetActive(false);
+                {
+                    // TODO: duplicite code
+                    _selectedObject.SetActive(false);
+
+                    if (previouslyHoveredObject) {
+                        Destroy(previouslyHoveredObject.GetComponent<Outline>());
+                        
+                        previouslyHoveredObject = null;
+                    }
+                }
             
             }
         }
         else
         {
-            pointerObject.SetActive(false);
+            // TODO: duplicite code
+            _selectedObject.SetActive(false);
+
+            if (previouslyHoveredObject) {
+                Destroy(previouslyHoveredObject.GetComponent<Outline>());
+                
+                previouslyHoveredObject = null;
+            }
         }
     }
 }
