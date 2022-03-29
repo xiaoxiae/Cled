61592652442eacc0885cd003a36c4b8d222f2495
xiaoxiae <tomas@slama.dev>
2022-03-29 14:06:28 +0200

    Better scene names + StateImportExportManager init

---
+++ Assets/StateImportExportManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateImportExportManager : MonoBehaviour
+{
+    public HoldStateManager holdStateManager;
+    public RouteManager routeManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    /// <summary>
+    /// Export the state from the given path.
+    /// </summary>
+    public void Import(string path)
+    {
+        
+    }
+
+    /// <summary>
+    /// Export the state to the given path.
+    /// </summary>
+    public void Export(string path)
+    {
+        
+    }
+}
