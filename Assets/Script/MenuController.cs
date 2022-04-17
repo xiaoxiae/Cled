985fd3bbbc6846c22c68f8a17a1d5fa8d44983a1
xiaoxiae <tomas@slama.dev>
2022-04-17 21:50:57 +0200

    Main menu continue path

--- Assets/Script/MenuController.cs
+++ Assets/Script/MenuController.cs
@@ -11,11 +11,25 @@ public class MenuController : MonoBehaviour
         var root = GetComponent<UIDocument>().rootVisualElement;
 
         var continueButton = root.Q<Button>("continue-button");
+        
+        var continuePathLabel = root.Q<Label>("continue-path-label");
 
-        if (PreferencesManager.LastOpenWallPath == "" || !File.Exists(PreferencesManager.LastOpenWallPath))
+        if (PreferencesManager.LastOpenWallPath == "" || !File.Exists(PreferencesManager.LastOpenWallPath)) {
             continueButton.SetEnabled(false);
+            continuePathLabel.style.display = DisplayStyle.None;
+            continuePathLabel.style.opacity = 100;
+        }
         else
+        {
             continueButton.clicked += () => SceneManager.LoadScene("WallScene");
+
+            string path = PreferencesManager.LastOpenWallPath;
+
+            if (path.Length > 50)
+                path = path[..25] + "..." + path[^25..];
+            
+            continuePathLabel.text = path;
+        }
 
         var openButton = root.Q<Button>("open-button");
         MenuUtilities.AddOpenButtonOperation(openButton);
