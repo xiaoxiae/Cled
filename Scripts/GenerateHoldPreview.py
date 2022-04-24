5021c93c1d180efe800e47a176a9e1cf715b6d42
xiaoxiae <tomas@slama.dev>
2022-04-24 12:10:11 +0200

    Hold preview: don't scale with camera distance

--- Scripts/GenerateHoldPreview.py
+++ Scripts/GenerateHoldPreview.py
@@ -57,9 +57,9 @@ parser.add_argument(
 parser.add_argument(
     "-l",
     "--light",
-    help="The distance of the light to the object, multiplied by the square root of the camera distance. Defaults to 30.",
+    help="The distance of the light to the object. Defaults to 30.",
     type=int,
-    default=12,
+    default=8,
 )
 
 arguments = parser.parse_args()
@@ -143,9 +143,9 @@ for step in range(rotation_steps + 1):
 
 # move the light relative to camera
 light.location = (
-    arguments.light * sqrt(max_dist),
-    -arguments.light * sqrt(max_dist),
-    arguments.light * sqrt(max_dist)
+    arguments.light,
+    -arguments.light,
+    arguments.light
 )
 
 # fit the view to the entire hold + scale it down a bit
